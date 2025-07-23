3e0a7739b4201493157b95a4aa825b63d93f9bd8
animeshDevarkar <156692896+animeshDevarkar@users.noreply.github.com>
2025-07-23 15:36:38 +0530

    Add files via upload

---
+++ project7-meme generator/index.js
@@ -0,0 +1,22 @@
+const generateMemeBtn = document.querySelector(".meme-generator .generate-meme-btn");
+const MemeImage = document.querySelector(".meme-generator img");
+const MemeTitle = document.querySelector(".meme-generator .meme-title");
+const MemeAuthor = document.querySelector(".meme-generator .meme-author");
+
+const updateDetails = (url,title,author)=>{
+    MemeImage.setAttribute("src",url);
+    MemeTitle.innerHTML = title;
+    MemeAuthor.innerHTML = author; 
+}
+
+
+const generateMeme = ()=>{
+    fetch("https://meme-api.com/gimme/wholesomememes")
+    .then((response)=>response.json())
+    .then(data => {
+        updateDetails(data.url , data.title , data.author)
+    });
+};
+
+
+generateMemeBtn.addEventListener("click",generateMeme)
